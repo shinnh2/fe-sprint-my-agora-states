89bfc1d8f6093658006d96b1a973d22bd0211d1a
shinnh2 <nahyeon.shin9@gmail.com>
2022-09-15 21:58:44 +0900

    script.js 디스커션 렌더링 코드 정리

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+//console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -23,8 +23,9 @@ const convertToDiscussion = (obj) => {
   
   //2. discussionContent - 내용 영역 div.discussion__content
   //innerHTML=`<h2 class="discussion__title"><a href="${obj.url}">${obj.title}</a></h2>`;
-  let elTitle=document.createElement('h2');
+  let elTitle=document.createElement('h3');
   let elTitleLink=document.createElement('a');
+  elTitle.className="discussion__title";
   elTitleLink.setAttribute('href',obj.url);
   elTitleLink.textContent=obj.title;
   elTitle.appendChild(elTitleLink);
@@ -36,11 +37,11 @@ const convertToDiscussion = (obj) => {
   discussionContent.append(elInfo);
 
   //3. 질문여부 삽입
-  //<p>☑</p>
-  let elAnswerChk=document.createElement('p');
-  elAnswerChk.textContent='☑';
+  let elAnswerChk=document.createElement('i');
+  elAnswerChk.className='material-icons disChecked';
+  elAnswerChk.textContent='check_box_outline_blank';
   discussionAnswered.append(elAnswerChk);
-  
+
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -57,3 +58,4 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
